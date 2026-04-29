2d813ddb5ba9af7301d113ad77cc58a2ad086281
Nikhil agrawal <99258446+Nikhil995@users.noreply.github.com>
2026-04-29 09:31:43 +0530

    Update Application.jsx

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "https://job-seeking-backend-6qav.onrender.com/api/v1/application/post",
+        "https://job-seeking-d82f.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
